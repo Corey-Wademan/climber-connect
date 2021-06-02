9546537b15594a23dad8f1aff7998904ebdd6486
Corey-Wademan <skaterboy699@gmail.com>
2021-06-02 14:10:25 -0400

    Setup config/db files

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 5000;
+const connectDb = require('./config/db');
+
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
 });
+
+connectDb();
 
 app.get('/', (req, res) => res.send('API Running'));
