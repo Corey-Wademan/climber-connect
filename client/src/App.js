27054e8212fb520af2007a808704ce8556e4b674
Corey Wademan <skaterboy699@gmail.com>
2021-06-16 12:04:54 -0400

    Added alert reducers/actions

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Layout/Navbar';
 import Landing from './Components/Layout/Landing';
 import Login from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
+import Alert from './Components/Layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store'
@@ -16,6 +17,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
