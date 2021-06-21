8c4ec638d12da97572519f60bdd64dba7eec77a2
Corey Wademan <skaterboy699@gmail.com>
2021-06-20 20:35:22 -0400

    Added redux logout logic

--- client/src/Components/Layout/Navbar.js
+++ client/src/Components/Layout/Navbar.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../../img/climb-logo.png'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {logout} from '../../actions/auth'
+ 
+const Navbar = ({ auth: { isAuth, loading }, logout }) => {
+  
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className='fas.fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  )
+  
+  const guestLinks = (
+    <ul>
+      <li><a href="#!">Climbers</a></li>
+      <li>
+          <Link to="/register">Register</Link>
+      </li>
+      <li>
+          <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  )
 
-const Navbar = () => {
     return (
-        <nav className="navbar bg-dark">
-      <h1>
-        <Link to="/">
-            <img className='logo' src={logo}/> Climbing Connecter
-        </Link>
-      </h1>
-      <ul>
-        <li><a href="profiles.html">Climbers</a></li>
-        <li>
-            <Link to="/register">Register</Link>
-        </li>
-        <li>
-            <Link to="/login">Login</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className="navbar bg-dark">
+        <h1>
+          <Link to="/">
+              <img className='logo' src={logo}/> Climbing Connecter
+          </Link>
+          </h1>
+        {!loading &&
+          (<Fragment>{isAuth ? authLinks : guestLinks}</Fragment>)}
+      </nav>
     )
 }
 
-export default Navbar
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {logout})(Navbar)
