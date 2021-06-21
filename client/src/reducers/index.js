4de3a7131aff71fc5d2c8c00034f62a44852da7f
Corey Wademan <skaterboy699@gmail.com>
2021-06-21 08:51:10 -0400

    Added profile reducer/actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import alert from './alert';
 import auth from "./auth";
+import profile from './profile'
 // Because this file is named index we can simply import this file into the store which combines all reducers from the reducers folder
 
 export default combineReducers( {
     alert,
-    auth
+    auth,
+    profile
 } );
