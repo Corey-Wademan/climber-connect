fd14fc9cb0ffbc0803995eda87066cd4b517aee5
Corey Wademan <skaterboy699@gmail.com>
2021-06-23 17:03:28 -0400

    Sertup actions for getting all profiles and profile by userid

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,11 +1,51 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { CLEAR_PROFILE, DELETE_ACCOUNT, GET_PROFILE, PROFILE_ERROR } from "./types";
+import { CLEAR_PROFILE, DELETE_ACCOUNT, GET_PROFILE, GET_PROFILES, PROFILE_ERROR } from "./types";
 
 //Get current users profile
 export const getCurrentProfile = () => async dispatch => {
    try {
       const res = await axios.get('/api/profile/me');
+
+      dispatch({
+         type: GET_PROFILE,
+         payload: res.data
+      });
+   } catch (error) {
+      dispatch({
+         type: PROFILE_ERROR,
+         payload: {
+            msg: error.response.statusText, status: error.response.status
+         }
+      })
+   }
+}
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+   dispatch({ type: CLEAR_PROFILE });
+
+   try {
+      const res = await axios.get('/api/profile/');
+
+      dispatch({
+         type: GET_PROFILES,
+         payload: res.data
+      });
+   } catch (error) {
+      dispatch({
+         type: PROFILE_ERROR,
+         payload: {
+            msg: error.response.statusText, status: error.response.status
+         }
+      })
+   }
+}
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+   try {
+      const res = await axios.get(`/api/profile/user/${userId}`);
 
       dispatch({
          type: GET_PROFILE,
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -21,5 +21,6 @@ export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 
 // Profile Functionality
 export const GET_PROFILE = 'GET_PROFILE';
+export const GET_PROFILES = 'GET_PROFILES';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const DELETE_ACCOUNT = 'DELETE_ACCOUNT';
