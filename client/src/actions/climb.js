7929d02b253f8aad299a2b85f2dfad5f4a953cf1
Corey Wademan <skaterboy699@gmail.com>
2021-08-04 09:39:29 -0400

    added all redux actions for climbs

--- client/src/actions/climb.js
+++ client/src/actions/climb.js
@@ -18,3 +18,45 @@ export const getClimbs = user_id => async dispatch => {
 		})
 	}
 };
+
+// Add climb
+export const addClimb = formData => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}
+
+	try {
+		const res = await axios.post('/api/climbs', formData, config);
+
+		dispatch({
+			type: ADD_CLIMB,
+			payload: res.data
+		});
+		dispatch(setAlert('Climb recorded', 'success'))
+	} catch (error) {
+		dispatch({
+			type: CLIMB_ERROR,
+			payload: {msg: error.response.statusText, status: error.response.status}
+		});
+	}
+};
+
+// Delete a climb
+export const deleteClimb = climb_id => async dispatch => {
+	try {
+		await axios.delete(`/api/climbs/${climb_id}`);
+
+		dispatch({
+			type: DELETE_CLIMB,
+			payload: climb_id
+		})
+		dispatch(setAlert('Climb Deleted', 'danger'))
+	} catch (error) {
+		dispatch({
+			type: CLIMB_ERROR,
+			payload: {msg: error.response.statusText, status: error.response.status}
+		});
+	}
+};
