93a7cb6d3bdafa41c4ed7f6c4492a162e9aae52f
Corey Wademan <skaterboy699@gmail.com>
2021-08-10 22:16:59 -0400

    add stats top to profile + some style

--- client/src/Components/charts/ProfileCharts.js
+++ client/src/Components/charts/ProfileCharts.js
@@ -7,13 +7,25 @@ import GeneralCharts from './GeneralCharts'
 
 const ProfileCharts = ({profile: {climbs}}) => {
 
+	const totalGrades = climbs.map(climb => climb.grade)
+	const totalStyles = climbs.map(climb => climb.style)
+
+	const calcMost = (arr) => {
+    return arr.sort((a,b) =>
+          arr.filter(v => v===a).length - arr.filter(v => v===b).length
+    ).pop();
+}
 	
-	console.log(climbs)
+	console.log()
 	return (
 		<div className='profile-chart-container'>
-			<h1>Stats</h1>
-			<div>
-				<h3>Routes Logged: {climbs.length}</h3>
+			<div className='stats-top'>
+				<h1>Stats</h1>
+				<div className='qk-stats'>
+					<h3>Routes Logged: {climbs.length}</h3>
+					<h3>Average Grade: {calcMost(totalGrades)}</h3>
+					<h3>Most Active In: {calcMost(totalStyles)}</h3>
+				</div>
 			</div>
 
 			<AboutChart climbs={climbs} />
