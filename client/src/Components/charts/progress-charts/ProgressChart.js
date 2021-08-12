22598c477c6a4b9d82e1d172b9600ec7faa25cff
Corey Wademan <skaterboy699@gmail.com>
2021-08-11 23:15:06 -0400

    finished graphs

--- client/src/Components/charts/progress-charts/ProgressChart.js
+++ client/src/Components/charts/progress-charts/ProgressChart.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import formatDate from '../../../utils/formatDate'
+import { sportUnits } from '../../profile-forms/ProfileSelectors'
+import {  LineChart,Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 const ProgressChart = ({climbs}) => {
 
@@ -22,18 +25,57 @@ const ProgressChart = ({climbs}) => {
 
 	// Filter and sorts boulder climbs
 	let g;
-	const boulderArr = climbs.filter(climb => climb.style === 'boulder').sort((a,b) => (g=s=>parseInt(s,32)%334+s)(a.grade)>g(b.grade)||-1)
+	const boulderTotal = climbs.filter(climb => climb.style === 'boulder').sort((a,b) => (g=s=>parseInt(s,32)%334+s)(a.grade)>g(b.grade)||-1)
 
 	// Finds hardest climb in an array
 	const calcHighest = (arr, date) => {
 		return arr.pop()
 	}
 
+	// Calculate topRope Units
+	const tickFormatter = arr => {
+		return arr.map(climb => climb)
+	}
+	
+	
+
+	const data = total.map((climb) => ({ name: formatDate(climb.date), Grade: climb.grade}));
+	console.log(data)
+
+
 	 
 	return (
-		<div>
-			
+		<div className='chart'>
+			<h3>Most Recent Climbs</h3>
+			<LineChart
+				width={500}
+				height={300}
+				data={data}
+				margin={{
+				top: 5,
+				right: 30,
+				left: 20,
+				bottom: 5
+				}}
+				>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="name" />
+				<YAxis 
+					type='category'
+					domain={['dataMin', 'dataMax']} 
+					ticks={['5.0', '5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7', '5.8', '5.9', '5.10', '5.11', '5.12', '5.13', '5.14', '5.15']}
+					dataKey="Grade"/>
+				<Tooltip />
+				<Legend />
+				<Line
+					type="monotone"
+					dataKey="Grade"
+					stroke="#8884d8"
+					activeDot={{ r: 8 }}
+				/>
+    	</LineChart>
 		</div>
+		
 	)
 }
 
--- client/src/Components/profile-forms/ProfileSelectors.js
+++ client/src/Components/profile-forms/ProfileSelectors.js
@@ -17,3 +17,5 @@ export const climbingTypes = [
 export const pitchAmount = [
    '1', '2', '3', '4', '5', '6', '7', '8', '9+'
 ];
+
+export const sportUnits = [5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 5.10, 5.11, 5.12, 5.13, 5.14, 5.15]
