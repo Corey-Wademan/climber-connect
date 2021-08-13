4fc2e769873256dfb03f7533c5820ff4f7e3cbd6
Corey Wademan <skaterboy699@gmail.com>
2021-08-12 20:09:21 -0400

    finish chart profile

--- client/src/Components/charts/progress-charts/ProgressChart.js
+++ client/src/Components/charts/progress-charts/ProgressChart.js
@@ -61,9 +61,12 @@ const ProgressChart = ({climbs}) => {
 					/>
 				<YAxis
 					interval={0}
-					type='category'
+					type='number'
+					scale='linear'
+					domain={['dataMin', 'dataMax']}
+					tick={[5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 5.10, 5.11, 5.12, 5.13, 5.14, 5.15]}
+					reversed='true'
 					dataKey='Grade'
-					ticks={["5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10", "5.11", "5.12", "5.13", "5.14", "5.15"]}
 					/>
 				<Tooltip />
 				<Legend />
