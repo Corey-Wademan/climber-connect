2a8aca3a346e53cc8bd31a264df5fc157dbdb7ca
Corey Wademan <skaterboy699@gmail.com>
2021-08-27 18:21:57 -0400

    added boulder chart / fixed profile style

--- client/src/Components/charts/progress-charts/ProgressChart.js
+++ client/src/Components/charts/progress-charts/ProgressChart.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import {  LineChart,Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 const ProgressChart = ({climbs}) => {
- 
+	const [currentView, setCurrentView] = useState('Lead')
 
 
 	// Filter sport / trad climbs between <5.10 && >5.10 then sorts, then concat into totalRopeArr
@@ -16,10 +16,15 @@ const ProgressChart = ({climbs}) => {
 
 	const boulderTotal = climbs.filter(climb => {
 		return (
-			climb.style !== 'sport' || climb.style !=='trad'
+			climb.style == 'boulder'
 			&& climb.grade === 'V0' || climb.grade === 'V1' || climb.grade === 'V2' || climb.grade === 'V3' || climb.grade === 'V4' || climb.grade === 'V5' || climb.grade === 'V6' || climb.grade === 'V7' || climb.grade === 'V8' || climb.grade === 'V9' || climb.grade === 'V10' || climb.grade === 'V11' || climb.grade === 'V12' || climb.grade === 'V13' || climb.grade === 'V14' || climb.grade === 'V15' || climb.grade === 'V16' || climb.grade === 'V17'
 		)
 	})
+
+	const handleView = e => {
+		e.preventDefault();
+		setCurrentView(e.target.value)
+	}
 
 	console.log(sportTotal)
 
@@ -53,45 +58,84 @@ const ProgressChart = ({climbs}) => {
 
 	 
 	return (
-		<Fragment>	
-			<div className='chart'>
-				<h3>Recently Logged Sport Climbs</h3>
-				<LineChart
-					width={800}
-					height={300}
-					data={sportData}
-					margin={{top: 5, right: 30, left: 20, bottom: 5}}
-				>
-				<CartesianGrid strokeDasharray="3 3"/>
-				<XAxis 
-					dataKey='name'
-					reversed='true'
-					interval={0}
-					label={{value: 'Date', position: 'insideBottom', offset: -5}}
-					/>
-				<YAxis
-					interval={0}
-					type='number'
-					scale='linear'
-					domain={['dataMin', 'dataMax']}
-					tick={[5.0, 5.01, 5.02, 5.03, 5.04, 5.05, 5.06, 5.07, 5.08, 5.09, 5.10, 5.11, 5.12, 5.13, 5.14, 5.15]}
-					dataKey='Grade'
-					/>
-				<Tooltip />
-				<Legend />
-				<Line
-					type="monotone"
-					dataKey="Grade"
-					stroke="#8884d8"
-					activeDot={{ r: 8 }}
-				/>
-				</LineChart>
-			</div>
-
 		<div>
-
+			<div className='row'>
+				<button className='btn btn-ocean' value='Lead' onClick={handleView}>Lead</button>
+				<button className='btn btn-ocean' value='Boulder' onClick={handleView}>Boulder</button>
+			</div>
+			{
+				currentView == 'Lead'
+				? (
+					<div className='chart'>
+						<h3>Recently Logged Sport Climbs</h3>
+						<LineChart
+							width={800}
+							height={300}
+							data={sportData}
+							margin={{top: 5, right: 30, left: 20, bottom: 5}}
+						>
+						<CartesianGrid strokeDasharray="3 3"/>
+						<XAxis 
+							dataKey='name'
+							reversed='true'
+							interval={0}
+							label={{value: 'Date', position: 'insideBottom', offset: -5}}
+							/>
+						<YAxis
+							interval={0}
+							type='number'
+							scale='linear'
+							domain={['dataMin', 'dataMax']}
+							tick={[5.0, 5.01, 5.02, 5.03, 5.04, 5.05, 5.06, 5.07, 5.08, 5.09, 5.10, 5.11, 5.12, 5.13, 5.14, 5.15]}
+							dataKey='Grade'
+							/>
+						<Tooltip />
+						<Legend />
+						<Line
+							type="monotone"
+							dataKey="Grade"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+						/>
+						</LineChart>
+					</div>
+				)
+				: (
+					<div className='chart'>
+						<h3>Recently Logged Boulder Climbs</h3>
+						<LineChart
+							width={800}
+							height={300}
+							data={boulderData}
+							margin={{top: 5, right: 30, left: 20, bottom: 5}}
+						>
+						<CartesianGrid strokeDasharray="3 3"/>
+						<XAxis 
+							dataKey='name'
+							reversed='true'
+							interval={0}
+							label={{value: 'Date', position: 'insideBottom', offset: -5}}
+							/>
+						<YAxis
+							interval={0}
+							type='category'
+							scale='auto'
+							tick={["V0", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17"]}
+							dataKey='Grade'
+							/>
+						<Tooltip />
+						<Legend />
+						<Line
+							type="monotone"
+							dataKey="Grade"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+						/>
+						</LineChart>
+					</div>
+				)
+			}	
 		</div>
-		</Fragment>
 		
 		
 	)
